4bc70749f3751190a1cc32099614a485a95f632f
Maksim Patsulo <patsulo.m@outlook.com>
2022-04-29 15:31:00 +0700

    Update bg-of-wrds-method.py

--- ml_modules/bg-of-wrds-method.py
+++ ml_modules/bg-of-wrds-method.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
-
+#Выбор датафрейма и столбца для анализа
 def get_word_stat(data_url = 'data.csv',name_column ='skill_name'):
     vectorizer = CountVectorizer()
     dataset = pd.read_csv(data_url)
     skills = dataset[name_column].values.tolist()
     bag = vectorizer.fit_transform(skills)
-    print(vectorizer.vocabulary_)
+    #print(vectorizer.vocabulary_)
+    #подсчет колличество повторений слова в документе
     out_data = vectorizer.vocabulary_
+    return out_data
 
-
-get_word_stat(data_url='web/dash/skills.csv')
+#print(get_word_stat(data_url='web/dash/skills.csv'))
